9634d3f121d74a438df1f933e6a648abe9c9b3b9
Jen-Chieh Shen <jcs090218@gmail.com>
2024-01-01 01:30:34 -0800

    chore: Update copyright year

--- ff-guard.el
+++ ff-guard.el
@@ -1,6 +1,6 @@
 ;;; ff-guard.el ---  Create parent directory for non-existent file  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Copyright (C) 2022-2024  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
